e94823fb700917b7caa279e4cc51984805babb81
Budhavarapu Srinivasulu <budhavarapusrinu67@gmail.com>
2026-04-15 21:52:10 +0530

    Time: 47 ms (62.92%), Space: 107.7 MB (5.47%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.java
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.java
@@ -1,11 +1,43 @@
 class Solution {
     public boolean repeatedSubstringPattern(String s) {
         int n = s.length();
-        for (int i = 1; i <= n / 2; i++) {
-            if (n % i == 0 && s.substring(0, i).repeat(n / i).equals(s)) {
-                return true;
+    
+        if(n < 2){
+            return false;
+        }
+
+        int index = 0;
+
+        String[] candid_repeated = new String[n];
+        String repeated = "";
+        for(int idx = n-1; idx >=n/2; idx--){
+            char curr = s.charAt(idx);
+
+            if(curr == s.charAt(0)){
+                repeated = s.substring(idx);
+                candid_repeated[index] = repeated;
+                index++;
             }
         }
+
+        if(candid_repeated.length == 0){
+            return false;
+        }
+
+        StringBuilder res = new StringBuilder();
+
+        for(String candid : candid_repeated){
+            if(candid != null){
+                for(int i=0; i<n/candid.length(); i++){
+                    res.append(candid);
+                }
+            }
+            if(res.toString().compareTo(s) == 0){
+                return true;
+            }
+            res = new StringBuilder();
+        }
+
         return false;
     }
 }
