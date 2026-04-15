2181ebdd9bb397115cf93b2714afd056ed2dbf5f
Budhavarapu Srinivasulu <budhavarapusrinu67@gmail.com>
2026-04-15 21:37:57 +0530

    Time: 5 ms (39.92%), Space: 43.1 MB (59.25%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
@@ -1,22 +1,21 @@
-
-
-public class Solution {
+class Solution {
     public boolean halvesAreAlike(String s) {
-        Set<Character> vowels = new HashSet<>();
-        vowels.add('a'); vowels.add('e'); vowels.add('i'); vowels.add('o'); vowels.add('u');
-        vowels.add('A'); vowels.add('E'); vowels.add('I'); vowels.add('O'); vowels.add('U');
-
-        int vowelsCount = 0;
-        int midIndex = s.length() / 2;
-
-        for (int i = 0; i < midIndex; i++) {
-            char charA = s.charAt(i);
-            char charB = s.charAt(midIndex + i);
-            if (vowels.contains(charA)) vowelsCount++;
-            if (vowels.contains(charB)) vowelsCount--;
+        s=s.toLowerCase();
+        int len=s.length();
+        int vowel=0;
+        char c;
+        for(int i=0;i<len/2;i++){
+            c=s.charAt(i);
+          if(c=='a' || c=='e' || c=='i'|| c=='o'||c=='u'){
+            vowel++;
+          }
+        } for(int i=len/2;i<len;i++){
+            c=s.charAt(i);
+          if(c=='a' || c=='e' || c=='i'|| c=='o'||c=='u'){
+            vowel--;
+          }
+         
         }
-
-        return vowelsCount == 0;
+         return vowel==0;
     }
 }
-
